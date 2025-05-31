84c0874b1c8c165efc8dd80c17e413e7dbedcff8
sandy29 <sandylinta8@gmail.com>
2025-05-31 14:00:11 +0800

    soal2.py

---
+++ soal2.py
@@ -0,0 +1,12 @@
+# Data
+nama_list = ['Ayu', 'Budi', 'Citra', 'Ayu']
+nama_tuple = ('Dina', 'Eko', 'Dina', 'Fajar')
+
+# Konversi
+print("List:", nama_list)
+print("List menjadi Set:", set(nama_list))
+print("Set menjadi List:", list(set(nama_list)))
+
+print("Tuple:", nama_tuple)
+print("Tuple menjadi Set:", set(nama_tuple))
+print("Set menjadi Tuple:", tuple(set(nama_tuple)))
